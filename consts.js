71b05857221900a3bf97e8c9ea8d5c3a4eec9afb
Roman Timashoff <timashoff@gmail.com>
2023-05-28 12:58:58 +0600

    feat: add sentence command and upd syntax

--- consts.js
+++ consts.js
@@ -9,7 +9,7 @@ export const COMMAND = {
     description: 'close the programm',
   },
   HELP: {
-    key: ['.help', '-help', '.h', '-h'],
+    key: ['.help', '-help', '.h', '-h', ':h'],
     description: 'print help info',
   },
   CLEAR: {
@@ -17,7 +17,7 @@ export const COMMAND = {
     description: 'clear the input history',
   },
   LITERARY: {
-    key: ['-l'],
+    key: ['-l', ':l'],
     description: 'make a sentence more literary',
     instruction: 'Rewrite the following sentence in a modern literary style and simplify it',
   },
@@ -43,7 +43,7 @@ export const COMMAND = {
       'Translate the following sentence into Chinese and provide the Pinyin transcription',
   },
   PINYIN: {
-    key: ['-p'],
+    key: ['-p', ':p'],
     description: 'show the Pinyin transcription',
     instruction: 'Provide the Pinyin transcription of the following sentence',
   },
@@ -56,6 +56,11 @@ export const COMMAND = {
     key: ['-cefr', ':cefr'],
     description: 'show CEFR levels',
     instruction: 'correlate the following words with CEFR levels of difficulty',
+  },
+  SENTENCE: {
+    key: [':s'],
+    description: 'create a simple sentence',
+    instruction: 'create a simple sentence using the following words',
   },
 }
 
