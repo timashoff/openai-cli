6ed8a433fc88b096af72db3094d669011dbe08a0
Roman Timashoff <timashoff@gmail.com>
2023-05-28 12:24:08 +0600

    refactor: upd command syntax

--- consts.js
+++ consts.js
@@ -13,7 +13,7 @@ export const COMMAND = {
     description: 'print help info',
   },
   CLEAR: {
-    key: ['.clear', '.c'],
+    key: ['.clear', '.c', ':c'],
     description: 'clear the input history',
   },
   LITERARY: {
@@ -22,24 +22,25 @@ export const COMMAND = {
     instruction: 'Rewrite the following sentence in a modern literary style and simplify it',
   },
   GRAMMAR: {
-    key: ['-g'],
+    key: ['-g', ':g'],
     description: 'check the grammar',
     instruction: 'Check the grammar and correct typos in the following sentence',
   },
   TRANSLATE: {
-    key: ['-t', '-ru'],
+    key: ['-t', ':ru', ':t'],
     description: 'translate into Russian',
     instruction: 'translate the following sentence into Russian',
   },
   ENGLISH: {
-    key: ['-e', '-п'],
+    key: ['-e', '-п', ':e'],
     description: 'translate into English',
     instruction: 'translate the following sentence into English',
   },
   CHINESE: {
-    key: ['-ch', '-c'],
+    key: ['-ch', ':ch'],
     description: 'translate into Chinese',
-    instruction: 'Translate the following sentence into Chinese and provide the Pinyin transcription',
+    instruction:
+      'Translate the following sentence into Chinese and provide the Pinyin transcription',
   },
   PINYIN: {
     key: ['-p'],
@@ -47,12 +48,12 @@ export const COMMAND = {
     instruction: 'Provide the Pinyin transcription of the following sentence',
   },
   TRANSCRIPTION: {
-    key: ['-tr'],
+    key: ['-tr', ':tr'],
     description: 'show the English transcription',
     instruction: 'show the English transcription of the following',
   },
   CEFR: {
-    key: ['-cefr'],
+    key: ['-cefr', ':cefr'],
     description: 'show CEFR levels',
     instruction: 'correlate the following words with CEFR levels of difficulty',
   },
